6bafaaf5a52a6f6dd0acb154ed1e54640931ab8a
Actisle17 <126648662+Actisle17@users.noreply.github.com>
2023-03-06 15:24:31 +0700

    Add files via upload

---
+++ Chuong2/chuong2.2.3.py
@@ -0,0 +1,6 @@
+print("Nhap hai canh ke cua tam giac vuong: ")
+a=int(input("a="))
+b=int(input("b="))
+import math
+x=math.sqrt((a+b)**2)
+print("canh huyen la:",x)
---
+++ Chuong2/chuong2.2.5.py
@@ -0,0 +1,5 @@
+n=int(input("So tien ban dau: "))
+k=int(input("So thang gui: "))
+t=float(input("Lai suat/ thang: "))
+m=n*(1+k*t)
+print('Voi so tien ban dau', str(n)  ',sau', str(k)  'thang gui')
